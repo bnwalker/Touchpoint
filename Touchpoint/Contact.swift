12802031b6894186a1f518fd66ed87db6493dba0
User <user@node-1w7jr9qpzlp7k59o2550maq9a.ipv6.telus.net>
2018-01-14 06:59:38 -0800

    Initialized class of contact, but not working on iPhone

---
+++ Touchpoint/Contact.swift
@@ -0,0 +1,54 @@
+//
+//  Contact.swift
+//  Touchpoint
+//
+//  Created by User on 2018-01-14.
+//  Copyright © 2018 TouchPoint. All rights reserved.
+//
+
+import UIKit
+
+class Contact {
+    
+    //MARK: Properties
+    
+    var name: String
+    var org: String
+    var phone: Int
+    var address1: String
+    var address2: String
+    var email: String
+    var frequency: String
+    var lastTPDate: Date
+    var birthday: Date
+    var note: String
+
+    //MARK: Initialization
+    
+    init?(name: String, org: String, phone: Int, address1: String, address2: String, email: String, frequency: String, lastTPDate: Date, birthday: Date, note: String) {
+        
+        // Initialization should fail if name is empty
+        if name.isEmpty {
+            return nil
+        }
+        
+        // Initiazation should fail if frequency is any value other than the established options
+        if !(frequency == "Monthy" || frequency == "Quarterly" || frequency == "Semiannually") {
+            return nil
+        }
+        
+        // Initialize stored properties
+        self.name = name
+        self.org = org
+        self.phone = phone
+        self.address1 = address1
+        self.address2 = address2
+        self.email = email
+        self.frequency = frequency
+        self.lastTPDate = lastTPDate
+        self.birthday = birthday
+        self.note = note
+    }
+    
+}
+
